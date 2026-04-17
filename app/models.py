b05273bf78fb6fbe6b519fe1b3146f3b8aa764ae
Ангелина Цой <angelrigay2001@gmail.com>
2026-04-17 16:57:32 +0300

    Docker

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, TIMESTAMP
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.database import Base
